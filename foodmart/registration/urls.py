3ff045c4e63d38e0fe0db3c4684aa25fc2b2618f
bhavanarao55 <65603114+bhavanarao55@users.noreply.github.com>
2022-10-18 19:17:03 -0400

    Delete foodmart directory

--- foodmart/registration/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home),
-    path('contact/',views.contact),
-    path('signup/', views.signup),
-    path('signin/', views.signin),
-]
